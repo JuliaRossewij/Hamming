3978aa8cb9a47d1d1b960b35d6dbed26280c9267
JuliaRossewij <j.f.rossewij@students.uu.nl>
2024-01-19 21:57:53 +0100

    read and prints string

--- hamming.py
+++ hamming.py
@@ -1,1 +1,11 @@
-print("Dit is een test")
+s = input ()
+string = ""
+
+
+for i in s :
+    binary = str(bin(ord(i)))[:5] + " " + str(bin(ord(i)))[5:] + " "
+    binary = binary.replace("b","")
+    string  = string + binary
+
+print (string)
+lijst = string.split(" ")
