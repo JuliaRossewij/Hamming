82dbb5024be64b1970b355588b420bfe55b88c85
JuliaRossewij <j.f.rossewij@students.uu.nl>
2024-01-24 22:14:15 +0100

    handling nibble in fuction and added most significant nibble

--- hamming.py
+++ hamming.py
@@ -34,94 +34,88 @@ def NibbleTo2DbitArray(nible):
         nible=nible>>1
     return M
 
+def AnalyseString(Istring, MS_LS, GM, HM, RM, ErrorFraction):
+    G=BinaryMatrix(GM)
+    H=BinaryMatrix(HM)
+    R=BinaryMatrix(RM)
+
+    yLSstring=""
+    yMSstring=""
+    Errstring=""
+    ErrorDetectString=""
+    DetectOkSting=""
+    DataCorrectString=""
+
+    TotalNbits=0
+    NflippedBits=0
+    Nnibbles=0
+    NwrongDetects=0
+    NwrongReceived=0
+    for element in Istring:
+        Nnibbles=Nnibbles+1
+        asci=(ord(element)) >> MS_LS
+#encode
+        xM=NibbleTo2DbitArray(asci)
+        x=BinaryMatrix(xM)
+        y=G*x
+#print encoded data
+        sum1=0
+        for n in range(y.Nrow):
+            sum1=sum1+y.M[n][0]*2**n
+        yLSstring=yLSstring+f"{sum1:02x}"[1]
+        yMSstring=yMSstring+f"{sum1:02x}"[0]
+#Introduce random Errors in data with parity
+        ErrorVector=random.choices([1, 0], weights=[ErrorFraction, 1-ErrorFraction], k=len(y.M))
+        NumberOfErrors=ErrorVector.count(1)
+        NflippedBits=NflippedBits+NumberOfErrors
+        TotalNbits=TotalNbits+len(y.M)
+        Errstring=Errstring+str(NumberOfErrors)
+        ErrorV=BinaryMatrix([ErrorVector])
+        ErrorM=ErrorV.Mt
+        Error=BinaryMatrix(ErrorM)
+        yError =Error+y
+#Error detect
+        ErrorDetect=H*yError
+        ErrorDetectString=ErrorDetectString+str(ErrorDetect.M[0][0])
+#Decode
+        yDecode=R*yError
+        if ((yDecode.M!=xM and ErrorDetect.M[0][0]==0) or (yDecode.M==xM and ErrorDetect.M[0][0]!=0) ):
+                DetectOkSting=DetectOkSting+'X'
+                NwrongDetects=NwrongDetects+1
+        else:
+                DetectOkSting=DetectOkSting+'-'
+        if (yDecode.M==xM):
+                DataCorrectString=DataCorrectString+'-'
+        else:
+                DataCorrectString=DataCorrectString+'X'
+                NwrongReceived=NwrongReceived+1
+
+    print('yLS  ', yLSstring)
+    print('yMS  ', yMSstring)
+    print('#Err ', Errstring,' = ',f"{(100*NflippedBits/TotalNbits):.3g}",'%')
+    print('ErDet', ErrorDetectString)
+    print('DetOk', DetectOkSting,' = ',f"{(100*NwrongDetects/Nnibbles):.3g}",'%')
+    print('DatOk', DataCorrectString,' = ',f"{(100*NwrongReceived/Nnibbles):.3g}",'%')
+    return
+
 GparM= [[1, 0, 0, 0],
 [0, 1, 0, 0],
 [0, 0, 1, 0],
 [0, 0, 0, 1],
 [1, 1, 1, 1]]
-Gpar=BinaryMatrix(GparM)
 
 HparM = [[1, 1, 1, 1, 1]]
-Hpar=BinaryMatrix(HparM)
 
 RparM= [[1, 0, 0, 0, 0],
 [0, 1, 0, 0, 0],
 [0, 0, 1, 0, 0],
 [0, 0, 0, 1, 0]]
-Rpar=BinaryMatrix(RparM)
-
-LsHexstring=""
-LsParstring=""
-LsErrstring=""
-MsHexstring=""
-MsParstring=""
-ParErrorDetectString=""
-ParDetectOkSting=""
-DataCorrectString=""
-
-TotalNbits=0
-NflippedBits=0
-Nnibbles=0
-NwrongDetects=0
-NwrongReceived=0
 
 ErrorFraction = float(input('Give fraction of bit errors (number between 0 and 1)'))
 Istring = input('Text :')
-for element in Istring:
-    Nnibbles=Nnibbles+1
-    asci=ord(element)
-    asciHex=hex(asci)
-    MsHexstring=MsHexstring+asciHex[2]
-    LsHexstring=LsHexstring+asciHex[3]
-#encode with parity
- #   print(hex(asci))
-    xM=NibbleTo2DbitArray(asci)
- #   print(xM)
-    x=BinaryMatrix(xM)
-    y=Gpar*x
-#print parity
-    parHex=hex(y.M[4][0])                                 
-    LsParstring=LsParstring+parHex[2]
-#Introduce random Errors in data with parity
-    ErrorVector=random.choices([1, 0], weights=[ErrorFraction, 1-ErrorFraction], k=len(y.M))
-    NumberOfErrors=ErrorVector.count(1)
-    NflippedBits=NflippedBits+NumberOfErrors
-    TotalNbits=TotalNbits+len(y.M)
-    LsErrstring=LsErrstring+str(NumberOfErrors)
-    ErrorV=BinaryMatrix([ErrorVector])
-    ErrorM=ErrorV.Mt
-    Error=BinaryMatrix(ErrorM)
-    yError =Error+y
-#Error detect with parity
-    ErrorDetect=Hpar*yError
-    ParErrorDetectString=ParErrorDetectString+str(ErrorDetect.M[0][0])
-#Decode with parity
-    yDecode=Rpar*yError
-    if ((yDecode.M!=xM and ErrorDetect.M[0][0]==0) or (yDecode.M==xM and ErrorDetect.M[0][0]!=0) ):
-            ParDetectOkSting=ParDetectOkSting+'X'
-            NwrongDetects=NwrongDetects+1
-    else:
-            ParDetectOkSting=ParDetectOkSting+'-'
-    if (yDecode.M==xM):
-            DataCorrectString=DataCorrectString+'-'
-    else:
-            DataCorrectString=DataCorrectString+'X'
-            NwrongReceived=NwrongReceived+1
-    
-    m=NibbleTo2DbitArray(asci>>4)
-    x=BinaryMatrix(m)
-    y=Gpar*x
-    parHex=hex(y.M[4][0])
-    MsParstring=MsParstring+parHex[2]
+print('parity')
+AnalyseString(Istring, 0, GparM, HparM, RparM, ErrorFraction)
+AnalyseString(Istring,  4, GparM, HparM, RparM, ErrorFraction)
+print('hamming7_4')
 
-print('LShex', LsHexstring)
-print('LSpar', LsParstring)
-print('#Err ', LsErrstring,' = ',f"{(100*NflippedBits/TotalNbits):.3g}",'%')
-print('ErDet', ParErrorDetectString)
-print('DetOk', ParDetectOkSting,' = ',f"{(100*NwrongDetects/Nnibbles):.3g}",'%')
-print('DatOk', DataCorrectString,' = ',f"{(100*NwrongReceived/Nnibbles):.3g}",'%')
-print('MShex', MsHexstring)
-print('MSpar', MsParstring)
-
-
-
+print('hamming8_4')
